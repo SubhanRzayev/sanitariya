4815135af27424efadb3d33e3db9ef08676f840c
SubhanRzayev <subhanrzayev97@gmail.com>
2021-11-24 23:52:08 +0400

    Add IP Address

--- Sanitariya/settings.py
+++ Sanitariya/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-z8q4whw#!#00wd^y0447@i+xt#t-bzzp_0o@s*p4_xx*884qpj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["www.sanitariya-gigiyena.az","sanitariya-gigiyena.az","161.35.144.82"]
 
 
 # Application definition
